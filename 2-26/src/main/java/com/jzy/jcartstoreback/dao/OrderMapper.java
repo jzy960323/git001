62e5e9da8fc8311eb748204a8d203a5656143db4
贾 <2536326629@qq.com>
2020-02-26 15:16:29 +0800

    接口实现

---
+++ 2-26/src/main/java/com/jzy/jcartstoreback/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.jzy.jcartstoreback.dao;
+
+import com.jzy.jcartstoreback.po.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Long orderId);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Long orderId);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
